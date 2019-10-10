fcfb44e630c684319a5757c3ec06bb28ebd78db7
richardyao <yaoyxsgf@gmail.com>
2019-10-10 18:37:13 +0800

    using main function

--- training/actual_code/print_rabbit_array.py
+++ training/actual_code/print_rabbit_array.py
@@ -2,16 +2,29 @@
 @author yaoxs@shukun.net
 @date 2019/10/10 17:51
 """
-count = int(input("请输入需要生成的斐波那契数列数量："))
-first_number = 1
-print(first_number)
-second_number = 1
-print(second_number)
-now_number = 0
-while count > 2:
-    count -= 1
-    now_number = first_number + second_number
-    print(now_number)
-    first_number = second_number
-    second_number = now_number
 
+
+def generate_rabbit_array(count=2):
+    array = []
+    first_number = 1
+    array.append(first_number)
+    second_number = 1
+    array.append(second_number)
+    now_number = 0
+    while count > 2:
+        count -= 1
+        now_number = first_number + second_number
+        array.append(now_number)
+        first_number = second_number
+        second_number = now_number
+    return array
+
+
+def main():
+    count = int(input("请输入需要生成的斐波那契数列数量："))
+    for value in generate_rabbit_array(count):
+        print(value)
+
+
+if __name__ == '__main__':
+    main()
